b665c85d4733479aed2efd38ebd0fb64e58389e7
axcentone <tom.hrdlicka89@gmail.com>
2025-12-04 13:14:13 +0100

    Add files via upload

---
+++ farmari.js
@@ -0,0 +1,732 @@
+window.locations = [
+  {
+    "id": 8,
+    "name": "Bohušovice nad Ohří",
+    "hh": 219,
+    "hc": 9,
+    "penetration": 4.10958904109589,
+    "lat": 50.49328,
+    "lng": 14.15079,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 9,
+    "name": "Braňany",
+    "hh": 389,
+    "hc": 68,
+    "penetration": 17.480719794344473,
+    "lat": 50.54302,
+    "lng": 13.70061,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 10,
+    "name": "Bruntál",
+    "hh": 4359,
+    "hc": 673,
+    "penetration": 15.439320945170913,
+    "lat": 49.98919,
+    "lng": 17.46209,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 11,
+    "name": "Budišov nad Budišovkou",
+    "hh": 315,
+    "hc": 45,
+    "penetration": 14.285714285714285,
+    "lat": 49.79548,
+    "lng": 17.62898,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 7,
+    "name": "Bílovec",
+    "hh": 449,
+    "hc": 20,
+    "penetration": 4.4543429844097995,
+    "lat": 49.7568,
+    "lng": 18.02183,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 5,
+    "name": "Bělá nad Radbuzou",
+    "hh": 269,
+    "hc": 44,
+    "penetration": 16.356877323420075,
+    "lat": 49.59172,
+    "lng": 12.7214,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 6,
+    "name": "Bělá pod Bezdězem",
+    "hh": 246,
+    "hc": 44,
+    "penetration": 17.88617886178862,
+    "lat": 50.50209,
+    "lng": 14.81431,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 19,
+    "name": "Choceň",
+    "hh": 840,
+    "hc": 220,
+    "penetration": 26.190476190476193,
+    "lat": 50.00215,
+    "lng": 16.22657,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 20,
+    "name": "Chrastava",
+    "hh": 585,
+    "hc": 117,
+    "penetration": 20.0,
+    "lat": 50.81709,
+    "lng": 14.96972,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 68,
+    "name": "Chropyně",
+    "hh": 957,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 49.35695,
+    "lng": 17.36445,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 72,
+    "name": "Chrudim 1",
+    "hh": 1066,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 49.95219,
+    "lng": 15.8143,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 73,
+    "name": "Chrudim 2",
+    "hh": 759,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 49.94027,
+    "lng": 15.80037,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 12,
+    "name": "Cvikov",
+    "hh": 729,
+    "hc": 229,
+    "penetration": 31.412894375857338,
+    "lat": 50.77753,
+    "lng": 14.63077,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 69,
+    "name": "Frýdlant",
+    "hh": 249,
+    "hc": 58,
+    "penetration": 23.293172690763054,
+    "lat": 50.92303,
+    "lng": 15.07528,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 16,
+    "name": "Hanušovice",
+    "hh": 619,
+    "hc": 113,
+    "penetration": 18.25525040387722,
+    "lat": 50.08099,
+    "lng": 16.9323,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 70,
+    "name": "Heřmanův Městec",
+    "hh": 332,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 49.94778,
+    "lng": 15.66087,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 17,
+    "name": "Horní Bříza",
+    "hh": 679,
+    "hc": 63,
+    "penetration": 9.278350515463918,
+    "lat": 49.8405,
+    "lng": 13.35451,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 18,
+    "name": "Horní Slavkov",
+    "hh": 1292,
+    "hc": 161,
+    "penetration": 12.461300309597522,
+    "lat": 50.13914,
+    "lng": 12.80886,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 71,
+    "name": "Hradec Králové - Věkoše",
+    "hh": 641,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 50.22745,
+    "lng": 15.8373,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 65,
+    "name": "Hulín",
+    "hh": 1130,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 49.317,
+    "lng": 17.46474,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 23,
+    "name": "Jindřichov",
+    "hh": 162,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 50.25264,
+    "lng": 17.51386,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 21,
+    "name": "Jílové u Děčína",
+    "hh": 528,
+    "hc": 14,
+    "penetration": 2.6515151515151514,
+    "lat": 50.76082,
+    "lng": 14.10631,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 22,
+    "name": "Jílové u Prahy",
+    "hh": 411,
+    "hc": 92,
+    "penetration": 22.38442822384428,
+    "lat": 49.89455,
+    "lng": 14.49612,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 24,
+    "name": "Kadaň",
+    "hh": 308,
+    "hc": 7,
+    "penetration": 2.272727272727273,
+    "lat": 50.37632,
+    "lng": 13.27287,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 25,
+    "name": "Karlovy Vary - Stará Role",
+    "hh": 2252,
+    "hc": 110,
+    "penetration": 4.8845470692717585,
+    "lat": 50.25035,
+    "lng": 12.83373,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 26,
+    "name": "Karlovy Vary - Tuhnice",
+    "hh": 1512,
+    "hc": 96,
+    "penetration": 6.349206349206349,
+    "lat": 50.22851,
+    "lng": 12.83644,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 27,
+    "name": "Klášterec nad Ohří",
+    "hh": 2059,
+    "hc": 173,
+    "penetration": 8.40213695968917,
+    "lat": 50.38583,
+    "lng": 13.16934,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 28,
+    "name": "Kraslice",
+    "hh": 1476,
+    "hc": 249,
+    "penetration": 16.869918699186993,
+    "lat": 50.32268,
+    "lng": 12.51716,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 29,
+    "name": "Krnov",
+    "hh": 2350,
+    "hc": 18,
+    "penetration": 0.7659574468085106,
+    "lat": 50.09191,
+    "lng": 17.70321,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 30,
+    "name": "Krupka",
+    "hh": 2504,
+    "hc": 235,
+    "penetration": 9.384984025559104,
+    "lat": 50.68553,
+    "lng": 13.86083,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 31,
+    "name": "Kynšperk nad Ohří",
+    "hh": 1130,
+    "hc": 220,
+    "penetration": 19.469026548672566,
+    "lat": 50.11896,
+    "lng": 12.53061,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 32,
+    "name": "Ledeč nad Sázavou",
+    "hh": 825,
+    "hc": 53,
+    "penetration": 6.424242424242424,
+    "lat": 49.69442,
+    "lng": 15.27717,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 33,
+    "name": "Lipník nad Bečvou 1",
+    "hh": 745,
+    "hc": 118,
+    "penetration": 15.838926174496645,
+    "lat": 49.52583,
+    "lng": 17.58073,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 34,
+    "name": "Lipník nad Bečvou 2",
+    "hh": 339,
+    "hc": 80,
+    "penetration": 23.59882005899705,
+    "lat": 49.52852,
+    "lng": 17.59932,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 80,
+    "name": "Luby",
+    "hh": 386,
+    "hc": 91,
+    "penetration": 23.57512953367876,
+    "lat": 50.2539625329044,
+    "lng": 12.404921509287504,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 35,
+    "name": "Mimoň 1",
+    "hh": 352,
+    "hc": 84,
+    "penetration": 23.863636363636363,
+    "lat": 50.66447,
+    "lng": 14.72616,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 36,
+    "name": "Mimoň 2",
+    "hh": 750,
+    "hc": 84,
+    "penetration": 11.200000000000001,
+    "lat": 50.65735,
+    "lng": 14.73452,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 37,
+    "name": "Mimoň 3",
+    "hh": 117,
+    "hc": 26,
+    "penetration": 22.22222222222222,
+    "lat": 50.64999,
+    "lng": 14.74065,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 38,
+    "name": "Mohelnice",
+    "hh": 1747,
+    "hc": 53,
+    "penetration": 3.033772180881511,
+    "lat": 49.77676,
+    "lng": 16.91911,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 39,
+    "name": "Nový Bor",
+    "hh": 1775,
+    "hc": 131,
+    "penetration": 7.380281690140846,
+    "lat": 50.75767,
+    "lng": 14.55921,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 40,
+    "name": "Odolena Voda",
+    "hh": 686,
+    "hc": 66,
+    "penetration": 9.620991253644315,
+    "lat": 50.23385,
+    "lng": 14.41155,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 41,
+    "name": "Odry",
+    "hh": 709,
+    "hc": 106,
+    "penetration": 14.950634696755994,
+    "lat": 49.66391,
+    "lng": 17.83091,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 42,
+    "name": "Orlová",
+    "hh": 827,
+    "hc": 11,
+    "penetration": 1.3301088270858523,
+    "lat": 49.87019,
+    "lng": 18.42665,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 43,
+    "name": "Ploužnice",
+    "hh": 388,
+    "hc": 107,
+    "penetration": 27.577319587628867,
+    "lat": 50.63462,
+    "lng": 14.76907,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 44,
+    "name": "Police nad Metují",
+    "hh": 389,
+    "hc": 14,
+    "penetration": 3.5989717223650386,
+    "lat": 50.53701,
+    "lng": 16.23325,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 45,
+    "name": "Polička",
+    "hh": 788,
+    "hc": 19,
+    "penetration": 2.4111675126903553,
+    "lat": 49.7156,
+    "lng": 16.26806,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 66,
+    "name": "Přelouč",
+    "hh": 1057,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 50.03902,
+    "lng": 15.56389,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 46,
+    "name": "Příbor",
+    "hh": 868,
+    "hc": 20,
+    "penetration": 2.3041474654377883,
+    "lat": 49.64164,
+    "lng": 18.14833,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 47,
+    "name": "Rotava",
+    "hh": 961,
+    "hc": 155,
+    "penetration": 16.129032258064516,
+    "lat": 50.29569,
+    "lng": 12.57343,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 48,
+    "name": "Sedlčany",
+    "hh": 1287,
+    "hc": 241,
+    "penetration": 18.725718725718725,
+    "lat": 49.66158,
+    "lng": 14.43309,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 49,
+    "name": "Semily 1",
+    "hh": 763,
+    "hc": 59,
+    "penetration": 7.732634338138926,
+    "lat": 50.60866,
+    "lng": 15.3281,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 50,
+    "name": "Semily 2",
+    "hh": 833,
+    "hc": 92,
+    "penetration": 11.044417767106843,
+    "lat": 50.60835,
+    "lng": 15.32633,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 51,
+    "name": "Skuteč",
+    "hh": 748,
+    "hc": 202,
+    "penetration": 27.00534759358289,
+    "lat": 49.84331,
+    "lng": 15.99633,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 52,
+    "name": "Světlá nad Sázavou",
+    "hh": 854,
+    "hc": 24,
+    "penetration": 2.810304449648712,
+    "lat": 49.66894,
+    "lng": 15.41025,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 55,
+    "name": "Trutnov 1",
+    "hh": 406,
+    "hc": 20,
+    "penetration": 4.926108374384237,
+    "lat": 50.58623,
+    "lng": 15.88617,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 56,
+    "name": "Trutnov 2",
+    "hh": 1690,
+    "hc": 176,
+    "penetration": 10.414201183431953,
+    "lat": 50.58735,
+    "lng": 15.88689,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 58,
+    "name": "Týnec nad Sázavou",
+    "hh": 717,
+    "hc": 77,
+    "penetration": 10.739191073919107,
+    "lat": 49.83389,
+    "lng": 14.59035,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 57,
+    "name": "Třebíč - Borovina",
+    "hh": 1239,
+    "hc": 243,
+    "penetration": 19.612590799031477,
+    "lat": 49.20715,
+    "lng": 15.85568,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 74,
+    "name": "Vamberk",
+    "hh": 796,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 50.11733,
+    "lng": 16.29379,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 75,
+    "name": "Veselí nad Moravou",
+    "hh": 1844,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 48.95328,
+    "lng": 17.37956,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 61,
+    "name": "Vrbno pod Pradědem 1",
+    "hh": 593,
+    "hc": 146,
+    "penetration": 24.620573355817875,
+    "lat": 50.11692,
+    "lng": 17.38352,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 62,
+    "name": "Vrbno pod Pradědem 2",
+    "hh": 730,
+    "hc": 136,
+    "penetration": 18.63013698630137,
+    "lat": 50.12022,
+    "lng": 17.3751,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 63,
+    "name": "Vrchlabí",
+    "hh": 644,
+    "hc": 95,
+    "penetration": 14.751552795031056,
+    "lat": 50.61671,
+    "lng": 15.60666,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 60,
+    "name": "Vítkov",
+    "hh": 1099,
+    "hc": 263,
+    "penetration": 23.930846223839854,
+    "lat": 49.77445,
+    "lng": 17.74895,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 59,
+    "name": "Větřní",
+    "hh": 694,
+    "hc": 143,
+    "penetration": 20.605187319884728,
+    "lat": 48.77442,
+    "lng": 14.28581,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 64,
+    "name": "Zlaté Hory",
+    "hh": 543,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 50.26396,
+    "lng": 17.39277,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 76,
+    "name": "Zlín - Bartošova čtvrť",
+    "hh": 261,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 49.22531,
+    "lng": 17.70762,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 13,
+    "name": "Český Krumlov",
+    "hh": 1023,
+    "hc": 9,
+    "penetration": 0.8797653958944283,
+    "lat": 48.81239,
+    "lng": 14.31642,
+    "manager": "David Průcha"
+  },
+  {
+    "id": 14,
+    "name": "Český Těšín - Hrabinská",
+    "hh": 1978,
+    "hc": 3,
+    "penetration": 0.15166835187057634,
+    "lat": 49.74945,
+    "lng": 18.61443,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 15,
+    "name": "Český Těšín - Svibice",
+    "hh": 1844,
+    "hc": 12,
+    "penetration": 0.6507592190889371,
+    "lat": 49.73633,
+    "lng": 18.61673,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 53,
+    "name": "Šternberk 1",
+    "hh": 1159,
+    "hc": 44,
+    "penetration": 3.7963761863675582,
+    "lat": 49.72408,
+    "lng": 17.29839,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 54,
+    "name": "Šternberk 2",
+    "hh": 681,
+    "hc": 27,
+    "penetration": 3.9647577092511015,
+    "lat": 49.72661,
+    "lng": 17.28748,
+    "manager": "Petr Klumpar"
+  },
+  {
+    "id": 67,
+    "name": "Žacléř",
+    "hh": 317,
+    "hc": 0,
+    "penetration": 0.0,
+    "lat": 50.6621,
+    "lng": 15.9108,
+    "manager": "David Průcha"
+  }
+];
